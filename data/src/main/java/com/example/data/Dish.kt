5a33428e98deb2746a6034cb42030237cc4187fa
sakky <sakkywho@gmailcom>
2024-01-28 20:10:09 +0300

    update structure of Dish data class

--- data/src/main/java/com/example/data/Dish.kt
+++ data/src/main/java/com/example/data/Dish.kt
@@ -7,26 +7,29 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Dish(
-    @PrimaryKey val Uri: Int,
+    @PrimaryKey val id : Int,
+    @ColumnInfo(name = "Uri") val Uri: String?,
     @ColumnInfo(name = "label") val label: String?,
     @ColumnInfo(name = "image") val image: String?,
+    @ColumnInfo(name = "images") val images: Array<Array<String>>?,
     @ColumnInfo(name = "source") val source: String?,
-    @ColumnInfo(name = "Url") val Url: String?,
+    @ColumnInfo(name = "url") val url: String?,
     @ColumnInfo(name = "shareas") val shareas: String?,
+    @ColumnInfo(name = "yield") val yield: Int?,
     @ColumnInfo(name = "dietLabels") val dietLabels: Array<String>?,
     @ColumnInfo(name = "healthLabels") val healthLabels: Array<String>?,
-    @ColumnInfo(name = "cautions") val cautions: String?,
+    @ColumnInfo(name = "cautions") val cautions: Array<String>?,
     @ColumnInfo(name = "ingredientLines") val ingredientLines: Array<String>?,
-    @ColumnInfo(name = "ingredient") val ingredient: Array<String>?,
-    @ColumnInfo(name = "calories") val calories: Int?,
-    @ColumnInfo(name = "totalWeight") val totalWeight: Int?,
-    @ColumnInfo(name = "Totaltime") val Totaltime: String?,
-    @ColumnInfo(name = "cousinType") val cousinType: String?,
-    @ColumnInfo(name = "mealType") val mealType: String?,
-    @ColumnInfo(name = "dishType") val dishType: String?,
-    @ColumnInfo(name = "totalNutrients") val totalNutrients: Array<String>?,
-    @ColumnInfo(name = "totalDaily") val totalDaily: Array<String>?,
-    @ColumnInfo(name = "digest") val digest: String?
+    @ColumnInfo(name = "ingredient") val ingredient: Array<Array<String>>?,
+    @ColumnInfo(name = "calories") val calories: Double?,
+    @ColumnInfo(name = "totalWeight") val totalWeight: Double?,
+    @ColumnInfo(name = "totalTime") val totalTime: Int?,
+    @ColumnInfo(name = "cousinType") val cousinType: Array<String>?,
+    @ColumnInfo(name = "mealType") val mealType: Array<String>?,
+    @ColumnInfo(name = "dishType") val dishType: Array<String>?,
+    @ColumnInfo(name = "totalNutrients") val totalNutrients: Array<Array<Triple<String, Double, String>>>?,
+    @ColumnInfo(name = "totalDaily") val totalDaily: Array<Array<Triple<String, Double, String>>>?,
+    @ColumnInfo(name = "digest") val digest: Array<Array<String>>?
     ) {
 
 
