ab9cd0f63333f72954427efba11ac506c305e044
sakky <sakkywho@gmailcom>
2024-02-05 21:45:30 +0300

    little commit for merge into master

--- presentation/src/main/java/com/example/schmecken/presentation/ui/FirstFragment.kt
+++ presentation/src/main/java/com/example/schmecken/presentation/ui/FirstFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.schmecken.R
-import com.example.data.DatabaseProvider
-import com.example.data.Dish
 
 
 class FirstFragment : Fragment() {
